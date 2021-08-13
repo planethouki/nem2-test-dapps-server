7679bc679f808167cc6c3c621904a8206ab95042
planet★箒星 <26595148+planethouki@users.noreply.github.com>
2021-08-13 20:07:00 +0900

    sample

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,8 +1,20 @@
 import { Handler } from "@netlify/functions";
 
+type Hoge = {
+    hoge: string
+}
+
 const handler: Handler = async (event, context) => {
+    console.log(event)
+    if (typeof event.body === "string") {
+        const parse: Hoge = JSON.parse(event.body)
+        console.log(parse)
+    }
     return {
         statusCode: 200,
+        headers: {
+            'content-type': "application/json"
+        },
         body: JSON.stringify({ message: "Hello World" }),
     };
 };
